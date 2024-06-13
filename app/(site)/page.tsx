a752c7a6b8ab0505c76bf0e4f2bbdf001e6cc5dd
Factve <me.facteverse@gmail.com>
2024-06-13 21:18:14 +0530

    third commit

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
     return ( 
@@ -7,7 +8,7 @@ export default function Home() {
             <Image alt="logo" height='48' width='48' className="mx-auto w-auto" src='/images/messenger-logo.png' />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
         </div>
-        {/*AuthForm*/}
+        <AuthForm />
       </div>
     );
   }
